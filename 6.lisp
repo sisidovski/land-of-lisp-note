9de1e2bd3e3125c3db9d1f71d0496523b45ab03f
sisidovski <arerreee@gmail.com>
2015-08-05 02:44:28 +0900

    complete chapter 6.5

--- 6.lisp
+++ 6.lisp
@@ -82,3 +82,11 @@
                             nil)
                  'string))
   (fresh-line))
+
+; 6.5 lambda
+(defun half (n)
+  (/ n 2))
+
+(lambda (n) (/ n 2))
+
+(mapcar (lambda (n) (/ n 2)) '(2 4 6))
