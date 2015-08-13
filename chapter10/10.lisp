24aebe2d4c47fffe8e2932017b7aa5007a3a9516
sisidovski <arerreee@gmail.com>
2015-08-14 03:50:26 +0900

    in the middle of chapter 10

---
+++ chapter10/10.lisp
@@ -0,0 +1,53 @@
+(loop for i
+      below 5
+      sum i)
+
+(loop for i
+      from 5
+      to 10
+      sum i)
+
+(loop for i
+      in '(100 20 3)
+      sum i)
+
+(loop for i
+      below 5
+      do (print i))
+
+(loop for i
+      below 10
+      when (oddp i)
+      sum i)
+
+(loop for i
+      from 0
+      do (print i)
+      when (= i 5)
+      return 'falafel)
+
+(loop for i
+      in '(2 3 4 5 6)
+      collect (* i i))
+
+(loop for x below 10
+      for y below 10
+      collect (+ x y))
+
+(loop for x below 10
+      collect (loop for y below 10
+                    collect (+ x y)))
+
+(loop for i
+      from 0
+      for day
+      in '(monday tuesday wednesday thursday saturday sunday)
+      collect (cons i day))
+
+; simulated evolution
+(defparameter *width* 100)
+(defparameter *height* 30)
+(defparameter *junble* '(45 10 10 10))
+(defparameter *plant-energy* 80)
+
+(defparameter *plants* (make-hash-table :test #'equal))
